7d19a8028e21e09bd3dc92d234bc19af0ce9eda2
MarioBlasi <mblasi509@gmail.com>
2023-06-14 14:45:54 +0100

    "./router.js";

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import App from "./App.vue";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
-
-createApp(App).mount("#app");
+import { router } from "./router.js";
+createApp(App).use(router).mount("#app");
